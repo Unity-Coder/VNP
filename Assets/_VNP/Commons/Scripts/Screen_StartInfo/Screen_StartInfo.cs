b23e533a80917c1f135a466dc0f387ec091f47a5
UnityCoder <pfreby@hotmail.com>
2019-01-24 15:53:57 +0100

    Screen Star

---
+++ Assets/_VNP/Commons/Scripts/Screen_StartInfo/Screen_StartInfo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VNP.Commons
+{
+    /// <summary>
+    /// Utilisable pour créer un écran d'information au lancement du jeu.
+    /// </summary>
+    public class Screen_StartInfo : MonoBehaviour
+    {
+        /// <summary>
+        /// Temps d'attente en seconde pour charger la scène suivante.
+        /// </summary>
+        [SerializeField]
+        private float _waitScreen = 2f;
+
+        /// <summary>
+        /// Nom de la scène à charger après le temps d'attente.
+        /// </summary>
+        [SerializeField]
+        private string _scene_Start = "";
+
+        /// <summary>
+        /// Start du composant.
+        /// </summary>
+        private void Start()
+        {
+            StartCoroutine(Wait(_waitScreen));
+        }
+
+
+        /// <summary>
+        /// Routine d'attente avant passage à la scène _scene_Start.
+        /// </summary>
+        /// <param name="time">Temps d'attente en secondes.</param>
+        /// <returns></returns>
+        private IEnumerator Wait(float time)
+        {
+            // Attend time secondes.
+            yield return new WaitForSeconds(time);
+
+            // Charge la prochaine scène.
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(_scene_Start);
+            while (!asyncLoad.isDone)
+                yield return null;
+        }
+    }
+}
